ab74c16f78dc0e1c5e262a172a958393e36d527a
Forsunkin <guazah@gmail.com>
2022-06-29 17:21:20 +0300

    123

--- test+.py
+++ test+.py
@@ -1,15 +1,10 @@
-from keys import steam_api
 import requests
 
-url_heroes = f'https://api.opendota.com/api/heroes'
-
-re = requests.get(url_heroes)
-re.json()[0]["id"], re.json()[0]["localized_name"]
-roles = re.json()[0]["roles"]
+url_public = 'https://api.opendota.com/api/publicMatches'
 
 
+re = requests.get(url_public)
+print(re.json()[0])
 
-c = ['Carry', 'Escape', 'Nuker']
-r = c + ([None] * (4 - len(c))) if len(c) < 4 else c
-print(r)
 
+'match_id',
