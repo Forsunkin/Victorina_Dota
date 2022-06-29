dbfc870bd8f10145ae955b5a638d7492b58d8e80
Forsunkin <guazah@gmail.com>
2022-06-29 11:23:13 +0300

    123

--- db/post_data.py
+++ db/post_data.py
@@ -16,11 +16,22 @@ def post_data_items():
     cur = conn.cursor()
     items = re_items.json()['result']['items']
 
-
     for item in items:
         uin = (item['id'], item['localized_name'])
         cur.execute(f'''INSERT INTO items VALUES (?,?)''', uin)
     conn.commit()
 
-def post_data_heroes():
 
+def post_data_heroes():
+    conn = sqlite3.connect('eto_baza.db')
+    cur = conn.cursor()
+    heroes = re_heroes.json()
+
+    for hero in heroes:
+        roles_list = hero["roles"]
+        roles = roles_list + ([None] * (4 - len(roles_list))) if len(roles_list) < 4 else roles_list
+        uin = roles[:4]
+        postal = hero["id"], hero["localized_name"], uin[0], uin[1], uin[2], uin[3]
+        cur.execute(f'''INSERT INTO heroes VALUES (?,?,?,?,?,?)''', postal)
+    conn.commit()
+post_data_heroes()
